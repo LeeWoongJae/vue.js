d63efc9d9df08254661e57c621a76f9035c53867
LeeWJ <dsa00340@naver.com>
2025-07-08 17:55:23 +0900

    commit

--- vue-project/src/views/ShowVue.vue
+++ vue-project/src/views/ShowVue.vue
@@ -4,6 +4,8 @@
   <button @click="toggleIf">v-if 토글</button>
   <p v-show="isShow">v-show를 사용하여 나타내기&nbsp;{{ isShow }}</p>
   <button @click="toggleShow">v-show 토글</button>
+  <!-- v-if는 초기에 영역을 차지하지 않지만 true값이 되면 그때 생성 -->
+  <!-- v-show는 영역이 차지하면서 보이거나 보이지 않거나 하는 강제 자리 차지-->
 </template>
 
 <script>
